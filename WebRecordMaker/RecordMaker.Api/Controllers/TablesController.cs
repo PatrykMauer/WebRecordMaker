81a9d9d47c6fbfa2ed6d91096d488d50833c0e5d
patryk.mauer <patryk.mauer@student.po.edu.pl>
2020-03-18 15:01:24 +0100

    Unit tests tu UserService added

--- WebRecordMaker/RecordMaker.Api/Controllers/TablesController.cs
+++ WebRecordMaker/RecordMaker.Api/Controllers/TablesController.cs
@@ -1,18 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
 using RecordMaker.Infrastructure.Commands;
 using RecordMaker.Infrastructure.Commands.Tables;
 using RecordMaker.Infrastructure.Commands.Users;
 using RecordMaker.Infrastructure.DTO;
 using RecordMaker.Infrastructure.Services;
-
 namespace RecordMaker.Api.Controllers
 {
     [ApiController]
-    [Microsoft.AspNetCore.Mvc.Route("[controller]")]
+    [Route("[controller]")]
     public class TablesController : ApiControllerBase
     {
         private readonly ITableService _tableService;
@@ -27,7 +25,7 @@ namespace RecordMaker.Api.Controllers
         public async Task<TableDto> Get(Guid id)
             => await _tableService.GetAsync(id);
 
-        [Microsoft.AspNetCore.Mvc.Route("all")]
+        [Route("all")]
         [HttpGet]
         public async Task<IEnumerable<TableDto>> GetAll()
             => await _tableService.GetAllAsync();
--- WebRecordMaker/RecordMaker.Tests/Services/UserServiceTests.cs
+++ WebRecordMaker/RecordMaker.Tests/Services/UserServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using AutoMapper;
 using FluentAssertions;
 using Moq;
@@ -12,7 +13,7 @@ namespace RecordMaker.Tests.Services
     public class UserServiceTests
     {
         [Fact]
-        public async Task register_async_should_invoke_add_async_on_repository()
+        public async Task register_async_should_invoke_add_async_get_async_on_repository()
         {
             var userRepositoryMock = new Mock<IUserRepository>();
             var mapperMock = new Mock<IMapper>();
@@ -22,17 +23,28 @@ namespace RecordMaker.Tests.Services
             
             userRepositoryMock.Verify(x=>x.AddAsync(It.IsAny<User>()),Times.Once);
         }
-
         [Fact]
-        public async Task registering_already_existing_user_should_throw_an_exception()
+        public async Task register_async_should_invoke_get_async_on_repository()
         {
             var userRepositoryMock = new Mock<IUserRepository>();
             var mapperMock = new Mock<IMapper>();
             
             var userService=new UserService(userRepositoryMock.Object,mapperMock.Object);
             await userService.RegisterAsync("obserer@wp.pl", "observ", "seceret", "Observer");
-            await userService.RegisterAsync("obserer@wp.pl", "observ", "seceret", "Observer");
-            //userRepositoryMock.Verify(???
+            
+            userRepositoryMock.Verify(x=>x.GetAsync(It.IsAny<string>()),Times.Once);
+        }
+
+        [Fact]
+        public async Task get_async_should_invoke_get_async_on_repository()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var mapperMock = new Mock<IMapper>();
+            
+            var userService=new UserService(userRepositoryMock.Object,mapperMock.Object);
+            await userService.GetAsync("email@email.com");
+            
+            userRepositoryMock.Verify(x=>x.GetAsync(It.IsAny<string>()),Times.Once());
         }
     }
 }
